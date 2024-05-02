3814861b16cd1ca71584b57ac76004d372a97703
lulultfh <punyalulu05@gmail.com>
2024-05-02 16:01:20 +0700

    add step 4

--- 209_latihan1.cpp
+++ 209_latihan1.cpp
@@ -28,5 +28,8 @@ void addNode() {
         }
         //if the list is empty, make the new Node the START
         newNode -> next = START; //step 3: make the new node point to the first node
+        if (START != NULL) {
+            START -> prev = newNode; // step 4: make the first node point to the new node
+        }
     }
 };
